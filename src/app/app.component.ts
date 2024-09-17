ff116f2c127a94428659c28ef52212f7d6c83a04
Tiago Santini <tiagosantiniathayde@outlook.com>
2024-09-17 16:07:57 -0300

    Implementada sessão sobre

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NgClass } from "@angular/common";
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
-  styles: [],
+  imports: [NgClass],
+  templateUrl: "app.component.html",
+  styleUrl: "app.component.scss",
 })
 export class AppComponent {
-  title = 'meu-portfolio';
+  public colapsarNavbar: boolean = false;
 }
