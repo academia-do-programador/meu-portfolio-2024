6c85598c396dfbb15cb662277233e217cff73512
Tiago Santini <tiagosantiniathayde@outlook.com>
2024-09-19 15:40:08 -0300

    Implementados componentes de seções

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from "@angular/core";
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { SobreComponent } from "./components/secoes/sobre/sobre.component";
+import { HabilidadesComponent } from "./components/secoes/habilidades/habilidades.component";
+import { StackComponent } from "./components/secoes/stack/stack.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [NavbarComponent, FooterComponent],
+  imports: [
+    NavbarComponent,
+    FooterComponent,
+    SobreComponent,
+    HabilidadesComponent,
+    StackComponent,
+  ],
   templateUrl: "app.component.html",
 })
 export class AppComponent {}
