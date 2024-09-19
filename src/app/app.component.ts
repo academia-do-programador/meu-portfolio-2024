ad7e7a81419ff4780dfa947d0aecce8506b73705
Tiago Santini <tiagosantiniathayde@outlook.com>
2024-09-19 15:16:43 -0300

    Adicionados componentes navbar e footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
-import { NgClass } from "@angular/common";
 import { Component } from "@angular/core";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [NgClass],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: "app.component.html",
-  styleUrl: "app.component.scss",
 })
-export class AppComponent {
-  public colapsarNavbar: boolean = false;
-}
+export class AppComponent {}
